7e89cc33c53057edf208b204a54b9be611d43192
TwoLargeGayMen <45555017+TwoLargeGayMen@users.noreply.github.com>
2026-02-17 13:33:08 -0600

    update to framework

--- Source/HeddiffGivesAbility.cs
+++ Source/HeddiffGivesAbility.cs
@@ -1,34 +1,74 @@
- using RimWorld;
+using RimWorld;
 using Verse;
-using System.Text;
-using System.Linq;
-using Core40k;
+using System.Reflection;
 using System.Collections.Generic;
- namespace Seg{
+using VEF.Abilities;
+
+namespace Seg
+{
     public class HediffComp_GivesAbility : HediffComp
-{
-    public HediffCompProperties_GivesAbility Props => (HediffCompProperties_GivesAbility)props;
-
-    public override void CompPostPostAdd(DamageInfo? dinfo)
     {
-        base.CompPostPostAdd(dinfo);
-        Pawn.abilities?.GainAbility(Props.ability);
+        public HediffCompProperties_GivesAbility Props => (HediffCompProperties_GivesAbility)props;
+
+        public override void CompPostPostAdd(DamageInfo? dinfo)
+        {
+            base.CompPostPostAdd(dinfo);
+
+            // VANILLA
+            if (Props.vanillaAbility != null)
+            {
+                Pawn.abilities?.GainAbility(Props.vanillaAbility);
+            }
+
+            // VEF
+            if (Props.vefAbility != null)
+            {
+                var comp = Pawn.GetComp<CompAbilities>();
+                comp?.GiveAbility(Props.vefAbility);
+            }
+        }
+
+        public override void CompPostPostRemoved()
+        {
+            base.CompPostPostRemoved();
+
+            // VANILLA
+            if (Props.vanillaAbility != null)
+            {
+                Pawn.abilities?.RemoveAbility(Props.vanillaAbility);
+            }
+
+            // VEF
+            if (Props.vefAbility != null)
+            {
+                var comp = Pawn.GetComp<CompAbilities>();
+                if (comp == null)
+                    return;
+
+                var field = typeof(CompAbilities).GetField(
+                    "learnedAbilities",
+                    BindingFlags.NonPublic | BindingFlags.Instance);
+
+                if (field == null)
+                    return;
+
+                var list = field.GetValue(comp) as List<VEF.Abilities.Ability>;
+                if (list == null)
+                    return;
+
+                list.RemoveAll(a => a.def == Props.vefAbility);
+            }
+        }
     }
 
-    public override void CompPostPostRemoved()
+    public class HediffCompProperties_GivesAbility : HediffCompProperties
     {
-        base.CompPostPostRemoved();
-        Pawn.abilities?.RemoveAbility(Props.ability);
+        public RimWorld.AbilityDef vanillaAbility;
+        public VEF.Abilities.AbilityDef vefAbility;
+
+        public HediffCompProperties_GivesAbility()
+        {
+            compClass = typeof(HediffComp_GivesAbility);
+        }
     }
 }
-
-public class HediffCompProperties_GivesAbility : HediffCompProperties
-{
-    public AbilityDef ability;
-
-    public HediffCompProperties_GivesAbility()
-    {
-        compClass = typeof(HediffComp_GivesAbility);
-    }
-}
- }
