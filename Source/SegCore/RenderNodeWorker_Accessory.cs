d75f35f5e15ac702afb983799c10ca97df4f35c0
TwoLargeGayMen <45555017+TwoLargeGayMen@users.noreply.github.com>
2026-08-08 09:33:33 -0500

    color

--- Source/SegCore/RenderNodeWorker_Accessory.cs
+++ Source/SegCore/RenderNodeWorker_Accessory.cs
@@ -1,8 +1,8 @@
+using System.Linq;
 using Core40k;
 using RimWorld;
 using Verse;
-using System.Linq;
-using Verse.AI;
+using UnityEngine;
 
 namespace seg
 {
@@ -19,51 +19,60 @@ namespace seg
 
             var worn = pawn.apparel.WornApparel;
 
-            ThingDef cargoPack  = DefDatabase<ThingDef>.GetNamed("Seg_Apparel_CargoPack",     false);
-            ThingDef medicaeBag = DefDatabase<ThingDef>.GetNamed("Seg_Apparel_MedicaeBag",    false);
-            ThingDef powerPack  = DefDatabase<ThingDef>.GetNamed("Seg_Apparel_PowerPack",     false);
-            ///ThingDef voxCaster  = DefDatabase<ThingDef>.GetNamed("Seg_Apparel_VoxCaster",     false);
-            ThingDef jumpPack   = DefDatabase<ThingDef>.GetNamed("Apparel_PackJump",          false);
+            ThingDef cargoPack  = DefDatabase<ThingDef>.GetNamed("Seg_Apparel_CargoPack",  false);
+            ThingDef medicaeBag = DefDatabase<ThingDef>.GetNamed("Seg_Apparel_MedicaeBag", false);
+            ThingDef powerPack  = DefDatabase<ThingDef>.GetNamed("Seg_Apparel_PowerPack",  false);
+            ThingDef jumpPack   = DefDatabase<ThingDef>.GetNamed("Apparel_PackJump",       false);
 
             bool hasCargo   = cargoPack  != null && worn.Any(a => a.def == cargoPack);
             bool hasMedical = medicaeBag != null && worn.Any(a => a.def == medicaeBag);
             bool hasPower   = powerPack  != null && worn.Any(a => a.def == powerPack);
-            ///bool hasComm    = voxCaster  != null && worn.Any(a => a.def == voxCaster);
             bool hasJump    = jumpPack   != null && worn.Any(a => a.def == jumpPack);
 
             if (ModsConfig.RoyaltyActive && hasJump)
                 backpackPath += "_Jump";
-
             if (hasCargo)
                 backpackPath += "_cargo";
-
             if (hasMedical)
                 backpackPath += "_medical";
-
             if (hasPower)
                 backpackPath += "_power";
-
             ///if (hasComm)
                /// backpackPath += "_communication";
 
             if (hasCargo && backpackPath.EndsWith("_cargo"))
                 cargoPack.graphicData.texPath = "";
-
             if (hasMedical && backpackPath.EndsWith("_medical"))
                 medicaeBag.graphicData.texPath = "";
-
             if (hasPower && backpackPath.EndsWith("_power"))
                 powerPack.graphicData.texPath = "";
-
             ///if (hasComm && backpackPath.EndsWith("_communication"))
                ///voxCaster.graphicData.texPath = "";
 
-            return GraphicDatabase.Get<Graphic_Multi>(
+
+            var multiColor = apparel.GetComp<CompMultiColor>();
+
+            string maskPath = null;
+            if (multiColor?.MaskDef != null)
+            {
+                maskPath = multiColor.MaskDef.maskPath;
+                if (multiColor.MaskDef.useBodyTypes && pawn.story?.bodyType != null)
+                    maskPath += "_" + pawn.story.bodyType.defName;
+            }
+
+            Shader shader = apparel.def.graphicData.shaderType.Shader;
+            if (multiColor?.Props != null && multiColor.Props.colorMaskAmount == 3)
+                shader = Core40kDefOf.BEWH_CutoutThreeColor.Shader;
+
+            return MultiColorUtils.GetGraphic<Graphic_Multi>(
                 backpackPath,
-                ShaderFor(pawn),
+                shader,
                 Props.drawSize,
-                apparel.DrawColor,
-                apparel.DrawColorTwo
+                multiColor?.DrawColor       ?? apparel.DrawColor,
+                multiColor?.DrawColorTwo    ?? apparel.DrawColorTwo,
+                multiColor?.DrawColorThree  ?? apparel.DrawColorTwo,
+                apparel.def.graphicData,
+                maskPath
             );
         }
     }
